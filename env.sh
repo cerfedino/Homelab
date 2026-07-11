16a9b0920350c0d55a04b9537385bed36bffb96f
Albert Cerfeda <39586706+cerfedino@users.noreply.github.com>
2026-07-11 15:24:46 +0000

    feat: relative repo root paths, shell completion

---
+++ env.sh
@@ -0,0 +1,13 @@
+
+export REPO_ROOT="$(git rev-parse --show-toplevel)"
+export TF_VAR_REPO_ROOT="$REPO_ROOT"
+
+export KUBECONFIG="$REPO_ROOT/20-bootstrap/kubeconfig.yaml"
+
+export SOPS_AGE_KEY_FILE="$REPO_ROOT/.sops/keys.txt"
+
+command -v pre-commit >/dev/null && [ ! -f "$(git rev-parse --git-path hooks/pre-commit 2>/dev/null)" ] && pre-commit install >/dev/null 2>&1 || true
+
+helm plugin list 2>/dev/null | grep -q '^diff' || helm plugin install --verify=false https://github.com/databus23/helm-diff >/dev/null 2>&1 || true
+
+[ -n "$BASH_VERSION" ] && case "$-" in *i*) eval 'source <(kubectl completion bash) && source <(helm completion bash) && source <(helmfile completion bash) && alias k=kubectl && complete -o default -F __start_kubectl k' ;; esac || true
